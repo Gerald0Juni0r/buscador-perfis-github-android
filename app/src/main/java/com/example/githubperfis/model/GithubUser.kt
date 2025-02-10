9e591801b1fb492045660ba7dc44311d37964588
Gerald0Juni0r <geraldo.gj63@gmail.com>
2025-02-10 13:26:48 -0300

    Criando estrutura MVVM + Incluindo classe de dados do usuário

---
+++ app/src/main/java/com/example/githubperfis/model/GithubUser.kt
@@ -0,0 +1,11 @@
+package com.example.githubperfis.model // Pacote do modelo
+
+data class GithubUser ( // Classe de dados do usuário no Github
+    val login: String, // Nome de usuário
+    val name: String?, // Nome real do usuário -> pode ser nulo?
+    val avatar_url: String, // URL da foto de perfil
+    val public_repos: Int, // Quantidade de repo. públicos
+    val followers: Int, // Quantidade de seguidores
+    val following: Int // Quantidade de usuários que está seguindo
+)
+
--- app/src/main/java/com/example/githubperfis/ui/MainActivity.kt
+++ app/src/main/java/com/example/githubperfis/ui/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.githubperfis
+package com.example.githubperfis.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.githubperfis.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
